8733331a9e4a3c8b4d32971f5d15b894d3d226f5
daijinming <daijinming@live.cn>
2017-02-08 14:51:04 +0800

    echarts地图
    echarts地图

---
+++ EChartsDemo.Tianjin/HomeModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace EChartsDemo.Tianjin
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+
+            Get[@"/"] = _ =>
+            {
+                var model = new { title = "", body = "" };
+
+                return View["Home.html", model];
+            };
+        }
+    }
+}
---
+++ EChartsDemo.Tianjin/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EChartsDemo.Tianjin.Startup))]
+namespace EChartsDemo.Tianjin
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+            app.UseNancy();
+        }
+    }
+}
